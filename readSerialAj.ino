c2ffeb9f5f2c8bca8a52ef4fe11def5927be484d
Aakash429405 <71318674+Aakash429405@users.noreply.github.com>
2021-10-24 12:25:25 +0530

    Add files via upload

---
+++ readSerialAj.ino
@@ -0,0 +1,17 @@
+
+int inByte=0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()>0){
+  inByte=Serial.read();
+Serial.print("I recieved: ");
+
+Serial.println(inByte,DEC);
+}
+}
